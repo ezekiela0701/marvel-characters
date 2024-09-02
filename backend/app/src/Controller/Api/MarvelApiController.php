bff946271c21f269e8867ff1474e71545192cc01
Ezekiela <ezekielaanderson@gmail.com>
2024-09-02 10:54:51 +0200

    Installation Vuejs Finish | Docker configuration vuejs Finish

---
+++ backend/app/src/Controller/Api/MarvelApiController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Services\Api\MarvelApiService as ApiMarvelApiService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MarvelApiController extends AbstractController
+{
+    private $marvelApi;
+
+    public function __construct(ApiMarvelApiService $marvelApi)
+    {
+        $this->marvelApi = $marvelApi;
+    }
+
+    #[Route('/api/marvel/characters', name: 'marvel_characters_list', methods: ['GET'])]
+    public function getCharacters(): Response
+    {
+
+        // Récupérer les paramètres de limite et offset depuis la requête
+        $limit = $_GET['limit'] ?? 20;
+        $offset = $_GET['offset'] ?? 0;
+
+        $characters = $this->marvelApi->getListCharacters('/characters' , $limit , $offset);
+
+        if ($characters) {
+            # code...
+            
+            return new JsonResponse([
+    
+                "data"      => $characters,
+                "code"      => Response::HTTP_OK,
+                "success"   => true,
+                "message"   => "Listing datas with success"
+    
+            ]);
+        }
+        else {
+            # code...
+            return new JsonResponse([
+                "data"      => [], 
+                "code"      => Response::HTTP_BAD_REQUEST , 
+                "success"   => false ,
+                "message"   => "No request found" ,
+        
+            ]);
+        }
+        
+    }
+}
---
+++ backend/app/src/Services/Api/MarvelApiService.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Services\Api;
+
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class MarvelApiService
+{
+    private $client;
+    private $param;
+    private $baseUrl;
+    private $publicKey;
+    private $privateKey;
+
+    public function __construct(HttpClientInterface $client, ParameterBagInterface $param)
+    {
+
+        $this->client       = $client;
+        $this->param        = $param ; 
+        $this->baseUrl      = $this->param->get('marvel_api_base_url') ;
+        $this->publicKey    = $this->param->get('marvel_api_public_key') ;
+        $this->privateKey   = $this->param->get('marvel_api_private_key') ;
+
+    }
+
+    public function getAuthParams(): array
+    {
+        $timestamp  = time();
+        $hash       = md5($timestamp . $this->privateKey . $this->publicKey);
+
+        return [
+            'ts'        => $timestamp,
+            'apikey'    => $this->publicKey,
+            'hash'      => $hash
+        ];
+    }
+
+    public function getListCharacters(string $endpoint,int $limit , int $offset)
+    {
+
+        // Fusion auth paramas with pagination
+        $queryParams    = array_merge($this->getAuthParams(), [
+            'limit'     => $limit,
+            'offset'    => $offset
+        ]);
+
+        $response = $this->client->request(
+            'GET',
+            $this->baseUrl . $endpoint,
+            [
+                'query' => $queryParams
+            ]
+        );
+
+        $datas =  $response->toArray();
+
+        // Fetch name and thumbnails only
+        $characters = [];
+        foreach ($datas['data']['results'] as $character) {
+
+            $characters[] = [
+                'name' => $character['name'],
+                'thumbnail' => $character['thumbnail']['path'] . '.' . $character['thumbnail']['extension']
+            ];
+
+        }
+
+        return $characters;
+
+    }
+}
